c0d71156223876d878870560218cb636a609b495
Maanvi10 <82942335+Maanvi10@users.noreply.github.com>
2022-03-15 17:34:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+       console.log(results);
+       console.log("nose x = " + results[0].pose.nose.x);
+       console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialised')
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot() {
+   save('Filter.png');
+}
